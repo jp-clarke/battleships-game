33552c7510df52f521dad1a6f6d163ec10f0f842
John Clarke <jp_clarke@outlook.com>
2023-03-09 21:51:16 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from beautifultable import BeautifulTable
+"""
+https://beautifultable.readthedocs.io/en/v0.7.0/
+"""
+from random import randint
+
+
+
+print("Welcome to Battleships!")
