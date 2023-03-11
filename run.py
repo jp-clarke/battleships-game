4a21f678004835c5e4cba23f34a4a519e6a49a4b
John Clarke <jp_clarke@outlook.com>
2023-03-11 15:17:34 +0000

    Add Board class and __init__ method

--- run.py
+++ run.py
@@ -4,6 +4,18 @@ https://beautifultable.readthedocs.io/en/v0.7.0/
 """
 from random import randint
 
+class Board:
+    """
+    Board class. Determines size of board, number of ships, name of player
+    and board type.
+    """
+    def __init__(self, board_size, ship_number, player_name, type):
+        self.board_size = board_size
+        self.ship_number = ship_number
+        self.player_name = player_name
+        self.type = type
+        self.guesses = []
+        self.ship_positions = []
 
 
 print("Welcome to Battleships!")
